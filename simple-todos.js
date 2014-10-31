64ca02485f6ebff05707507f5d3cd5e4082890e1
Santos Jiménez Linares <sjimenez77@gmail.com>
2014-10-31 10:23:32 +0100

    Adding tasks with a form
    - Sorting our tasks

--- simple-todos.js
+++ simple-todos.js
@@ -5,7 +5,27 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  // In the client code, right after Template.body.helpers:
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // This function is called when the new task form is submitted
+
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
     }
   });
 }
