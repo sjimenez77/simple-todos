a277109b253b1b75db961f1ad52bba5a717b2b16
Santos Jiménez <sjimenez77@gmail.com>
2014-10-31 14:46:32 +0100

    Adding user accounts

--- simple-todos.js
+++ simple-todos.js
@@ -31,7 +31,9 @@ if (Meteor.isClient) {
 
       Tasks.insert({
         text: text,
-        createdAt: new Date() // current time
+        createdAt: new Date(),            // current time
+        owner: Meteor.userId(),           // _id of logged in user
+        username: Meteor.user().username  // username of logged in user
       });
 
       // Clear form
@@ -55,5 +57,10 @@ if (Meteor.isClient) {
     "click .delete": function () {
       Tasks.remove(this._id);
     }
+  });
+  
+  // At the bottom of the client code
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 }
