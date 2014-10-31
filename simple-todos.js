039dc7149511d086c64dc6a7175fb7c11b9be85b
Santos Jiménez <sjimenez77@gmail.com>
2014-10-31 14:37:57 +0100

    Storing temporary UI state in Session
    - Showing a count of incomplete tasks

--- simple-todos.js
+++ simple-todos.js
@@ -2,10 +2,23 @@
 Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
-  // This code only runs on the client
+  // Replace the existing Template.body.helpers
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    // Add to Template.body.helpers
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
@@ -26,6 +39,10 @@ if (Meteor.isClient) {
 
       // Prevent default form submit
       return false;
+    },
+    // Add to Template.body.events
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
